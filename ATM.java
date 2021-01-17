de62bb7fe411524e63afa1012c2565e8a02d7a08
SahithiGujjula <70298866+SahithiGujjula@users.noreply.github.com>
2021-01-17 13:28:01 -0800

    Add files via upload

---
+++ ATM.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.io.IOException;
+
+public class ATM extends OptionMenu {
+
+    public static void main(String[] args) throws IOException {
+	OptionMenu optionMenu = new OptionMenu();
+	optionMenu.getLogin();
+    }
+}
